42cb12da8b61704a89e0a18081e0adeb93c34ac4
Mitch Brown <mitchbrown1024@gmail.com>
2025-02-08 11:51:44 -0800

    added navbar to layout component that persists on every page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
 	import NameTitle from "./name_title.svelte";
-    import Navbar from "$lib/components/navbar.svelte";
 </script>
 
-<Navbar></Navbar>
 <NameTitle></NameTitle>
--- src/routes/name_title.svelte
+++ src/routes/name_title.svelte
@@ -2,22 +2,6 @@
 <h2>Test light</h2>
 
 <style>
-    @font-face {
-        font-family: Cocogoose;
-        font-style: normal;
-        font-display: swap;
-        font-weight: bold;
-        src: url($lib/fonts/cocogoose.ttf) format("truetype");
-    }
-
-    @font-face {
-        font-family: Cocogoose;
-        font-style: normal;
-        font-display: swap;
-        font-weight: lighter;
-        src: url($lib/fonts/cocogoose-thin.ttf) format("truetype");
-    }
-
     h1 {
         font-family: Cocogoose;
         font-weight: bold;
