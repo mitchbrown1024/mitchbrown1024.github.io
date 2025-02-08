6f2d7df8dc011bfca64cc4815bc7f8bd67efa39a
Mitch Brown <mitchbrown1024@gmail.com>
2025-02-08 09:05:51 -0800

    Deleted default Home page html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,5 @@
 	import NameTitle from "./name_title.svelte";
 
 </script>
-<h1>Welcome to Svelte 5</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <NameTitle></NameTitle>
