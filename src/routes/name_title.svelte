15aac87a68c9b890b91ad1b98fd737e1e8664b77
Mitch Brown <mitchbrown1024@gmail.com>
2025-02-07 23:35:44 -0800

    Added font to CSS

---
+++ src/routes/name_title.svelte
@@ -0,0 +1,9 @@
+<style>
+    @font-face {
+        font-family: Cocogoose;
+        font-style: Regular;
+        font-display: swap;
+        font-weight: bold;
+        src: url($lib/fonts/cocogoose.ttf);
+    }
+</style>
