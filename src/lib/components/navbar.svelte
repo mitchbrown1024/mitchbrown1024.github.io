576ec6d8417c216479375b3ec293e393820860c3
Mitch Brown <mitchbrown1024@gmail.com>
2025-02-08 09:47:33 -0800

    Created basic navbar component and routes for new pages

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,5 @@
+<nav>
+	<a href="/">about</a>
+	<a href="/projects">projects</a>
+    <a href="/experience">experience</a>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import NameTitle from "./name_title.svelte";
-
+    import Navbar from "$lib/components/navbar.svelte";
 </script>
 
+<Navbar></Navbar>
 <NameTitle></NameTitle>
