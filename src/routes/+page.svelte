4328f10282ca8a51dc99f018d12513eacb371a37
Mitch Brown <mitchbrown1024@gmail.com>
2025-02-07 23:38:14 -0800

    Added test text to component and put component on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import NameTitle from "./name_title.svelte";
+
+</script>
 <h1>Welcome to Svelte 5</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<NameTitle></NameTitle>
--- src/routes/name_title.svelte
+++ src/routes/name_title.svelte
@@ -1,3 +1,5 @@
+<h1>Test</h1>
+
 <style>
     @font-face {
         font-family: Cocogoose;
@@ -5,5 +7,10 @@
         font-display: swap;
         font-weight: bold;
         src: url($lib/fonts/cocogoose.ttf);
+    }
+
+    h1 {
+        font-family: Cocogoose;
+        font-size: 128px;
     }
 </style>
