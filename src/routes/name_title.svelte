5d5258119264e95e62fa5eef848757d9c4bdea00
Mitch Brown <mitchbrown1024@gmail.com>
2025-02-08 09:12:23 -0800

    Added light version of the font

--- src/routes/name_title.svelte
+++ src/routes/name_title.svelte
@@ -1,16 +1,32 @@
 <h1>Test</h1>
+<h2>Test light</h2>
 
 <style>
     @font-face {
         font-family: Cocogoose;
-        font-style: Regular;
+        font-style: normal;
         font-display: swap;
         font-weight: bold;
-        src: url($lib/fonts/cocogoose.ttf);
+        src: url($lib/fonts/cocogoose.ttf) format("truetype");
+    }
+
+    @font-face {
+        font-family: Cocogoose;
+        font-style: normal;
+        font-display: swap;
+        font-weight: lighter;
+        src: url($lib/fonts/cocogoose-thin.ttf) format("truetype");
     }
 
     h1 {
         font-family: Cocogoose;
+        font-weight: bold;
         font-size: 128px;
+    }
+
+    h2 {
+        font-family: Cocogoose;
+        font-weight: lighter;
+        font-size: 64px;
     }
 </style>
