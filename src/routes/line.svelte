f3b78ff488d6d1391ce46f78692bebde65351ad8
Mitch Brown <mitchbrown1024@gmail.com>
2025-02-10 14:40:30 -0800

    chaged line svg to svelte component and added variables for height and width

---
+++ src/routes/line.svelte
@@ -0,0 +1,7 @@
+<script>
+    let { width, height } = $props();
+</script>
+
+<svg width={width} height={height} viewBox="0 0 954 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4.5 19.9606C70.3037 3.12715 1025.22 -10.6457 944.116 39.3447" stroke="white" stroke-width="8.1617" stroke-linecap="round"/>
+</svg>
