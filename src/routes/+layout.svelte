ab793dc48f27ac346a4240dd8aba72e6118bc5de
Mitch Brown <mitchbrown1024@gmail.com>
2025-02-10 20:57:57 -0800

    added hover effect to socials icons and increased spacing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	let { children } = $props();
     let icon_size = 48;
+    let github_color = $state("black");
+    let instagram_color = $state("black");
+    let linkedin_color = $state("black");
 	import { page } from '$app/state';
 	import { onMount } from 'svelte';
 	import Github from './github.svelte';
@@ -33,10 +36,31 @@
             </div>
         </div>
         <div class="empty"></div>
-        <div class="icons">
-            <Github size={icon_size}></Github>
-            <Instagram size={icon_size}></Instagram>
-            <LinkedIn size={icon_size}></LinkedIn>
+        <div class="icon-container">
+            <button class="icon" onmouseenter={() => {
+                    github_color="white";
+                }}
+                onmouseleave={() => {
+                    github_color="black";
+                }}>
+                <Github size={icon_size} color={github_color}></Github>
+            </button>
+            <button class="icon" onmouseenter={() => {
+                    instagram_color="white";
+                }}
+                onmouseleave={() => {
+                    instagram_color="black";
+                }}>
+                <Instagram size={icon_size} color={instagram_color}></Instagram>
+            </button>
+            <button class="icon" onmouseenter={() => {
+                    linkedin_color="white";
+                }}
+                onmouseleave={() => {
+                    linkedin_color="black";
+                }}>
+                <LinkedIn size={icon_size} color={linkedin_color}></LinkedIn>
+            </button>
         </div>
     </div>
 </div>
@@ -118,6 +142,9 @@
         display: flex;
         height: 100%;
     }
+    div.icon-container {
+        display: flex;
+    }
 
     div.empty {
         height: 20px;
@@ -127,6 +154,13 @@
         display: flex;
         flex-direction: column;
         justify-content: flex-start;
+    }
+    button.icon {
+        margin-right: 15px;
+        background: none;
+        border: none;
+        width: fit-content;
+        padding: 0;
     }
 
     :global(body) { /* this will apply to <body> */ margin: 0; padding: 0; }
