8e978e3405731274a956a6a66a07124ccaf90df7
Mitch Brown <mitchbrown1024@gmail.com>
2025-02-10 19:19:23 -0800

    added blue circle gradient for background of page

---
+++ src/routes/background_circle.svelte
@@ -0,0 +1,11 @@
+<span class="circle"></span>
+
+<style>
+    span.circle {
+        height: 2000px;
+        width: 2000px;
+        background: radial-gradient(#0496FF 700px, rgba(255, 255, 255, 0) 1250px);
+        display: inline-block;
+        filter: blur(100px);
+    }
+</style>
